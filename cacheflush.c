1478ff7da9b264300372548fe250ba12e5ff1d36
cpentra1 <cpentra1@gmail.com>
2013-12-16 19:56:10 -0500

    updating c programs (cacheflush and getclocktime)

--- cacheflush.c
+++ cacheflush.c
@@ -3,12 +3,11 @@
 #include <sys/syscall.h>
 
 int main() {
-  // from strace.. can't find man page w/ 5 args
-  // cacheflush(0x47f7c950, 0x47f7c954, 0, 0x950, 0x47f7c950)
   char testArray [100];  
-
+  unsigned long long i;
+  
   for(i=0; i<5000000000; i++) {
-    sys_cacheflush(&testArray, &testArray+100, 0);//ICACHE);
+    syscall(__ARM_NR_cacheflush, &testArray, 100, 0);
   }
   return 0;
 }
--- getclocktime.c
+++ getclocktime.c
@@ -7,12 +7,22 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+void safe_inc( unsigned long long *p )
+{
+  unsigned long long v = *p;
+  (*p)++;
+  if( v > *p )
+    exit( -1 );
+}
+
 int main() {
   struct timespec ts;
   unsigned long long i;
-  
+ 
+  /*  c11 precise bit uint64_t */  
   //750,000,000 calls
   for(i=0; i<750000000; i++) {
+    safe_inc(&i);
     syscall(SYS_clock_gettime, CLOCK_REALTIME, &ts);
   }
   return EXIT_SUCCESS;
